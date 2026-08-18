0cdc86536c65434e5180b13b88ad7f6f0f37b469
rakesh-koduri <rakeshkodur@069@gmail.com>
2026-08-18 17:01:50 +0530

    Fix import paths

--- frontend/src/Pages/Projects.jsx
+++ frontend/src/Pages/Projects.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import api from '../services/api'
+import api from '../Services/api'
 
 function Projects() {
   const navigate = useNavigate()
